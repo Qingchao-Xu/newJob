fbba7fb8ad08c1385ee2d6e300f07c8c0c91decd
Qingchao-Xu <15154739019@163.com>
2024-04-20 21:14:36 +0800

    init

---
+++ src/main/java/org/xu/newjob/NewJobApplication.java
@@ -0,0 +1,13 @@
+package org.xu.newjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/xu/newjob/NewJobApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xu.newjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
