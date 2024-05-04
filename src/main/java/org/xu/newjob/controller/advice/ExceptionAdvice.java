cd66c72523e513a9fec45cea158b0468957d7a7a
Qingchao-Xu <15154739019@163.com>
2024-05-04 21:04:51 +0800

    add ExceptionAdvice

---
+++ src/main/java/org/xu/newjob/controller/advice/ExceptionAdvice.java
@@ -0,0 +1,35 @@
+package org.xu.newjob.controller.advice;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.xu.newjob.util.NewJobUtil;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@ControllerAdvice(annotations = Controller.class)
+public class ExceptionAdvice {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionAdvice.class);
+
+    @ExceptionHandler({Exception.class})
+    public void handleException(Exception e, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        logger.error("服务器发生异常: " + e.getMessage());
+        for (StackTraceElement element : e.getStackTrace()) {
+            logger.error(element.toString());
+        }
+        String xRequestedWith = request.getHeader("x-requested-with");
+        if ("XMLHttpRequest".equals(xRequestedWith)) {
+            response.setContentType("application/plain;charset=utf-8");
+            PrintWriter writer = response.getWriter();
+            writer.write(NewJobUtil.getJSONString(1, "服务器异常！"));
+        } else {
+            response.sendRedirect(request.getContextPath() + "/error");
+        }
+    }
+}
